731950376d29fb2a8a4793098dcd3ec970f973f2
Kamil Musiał <kmusial40@gmail.com>
2020-10-22 12:43:36 +0200

    :+1:  Create a constructor for RankedGradeBook

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,7 +1,15 @@
+using System;
+using GradeBook.Enums;
+
 namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
-        
+        //TODO: resolve problem with GradeBookType.Ranked :<
+        public GradeBookType Type;
+        public RankedGradeBook(string name) : base(name)
+        {
+            
+        }
     }
 }
